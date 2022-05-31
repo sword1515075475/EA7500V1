62c6129c4de0fc728c7fe02ba149253b45798478
sword1515075475 <51266050+sword1515075475@users.noreply.github.com>
2022-05-31 09:58:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,9 +13,9 @@
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #sirpdboy
-git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
-#git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
+git clone https://github.com/deplives/luci-app-turboacc.git package/luci-app-turboacc
+git clone https://github.com/chinattw/luci-app-vlmcsd.git package/luci-app-vlmcsd
+#git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
 #git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 #git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 #git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
